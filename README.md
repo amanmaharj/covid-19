84477df3a4c079e9d0e5e92b960a1ae575924798
Aman Maharjan <amanmaharjan@Amans-MacBook-Air.local>
2021-02-05 04:26:46 +0545

    Changes to the css and update on md file

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # covid-19
-Gives you a data on covid-19 cases.
+Gives you a data on covid-19 cases (Total covid cases,Deaths, Recovered) of all countries
+
+Gives you a data on covid-19 cases of specific country on request.
+
+(The data are updated every day @ 22:10(GMT time))
+
+
+# Technology Used
+
+-HTML
+-CSS
+-Javascript
+
+# User Storiess
+
+Heatlth worker, Goverment bodies could use this service to retrieve the information for analyse and planning purpose.
+
+General People could use this service to get the updates on the the latest covid-19 cases.
+
+# Wire Framework
--- js/main.js
+++ js/main.js
@@ -1,40 +1,91 @@
 
-function listing(data){
-
-const list = document.createElement('li')
-
-list.textContent = data
-
-document.getElementById("detail").appendChild(list);
-
-
-}
-
-function clearUl(){
-   location.reload()
-}
-
-
-
+//Used to fetch the data from the api to display into the table format in the body
 fetch("https://covid-193.p.rapidapi.com/statistics", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "de8ca97b57mshbc75b832708333fp1b73aejsn44cd7debaa77",
-		"x-rapidapi-host": "covid-193.p.rapidapi.com"
-	}
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "de8ca97b57mshbc75b832708333fp1b73aejsn44cd7debaa77",
+        "x-rapidapi-host": "covid-193.p.rapidapi.com"
+    }
 })
-.then((response)=>{
-   return response.json()
+    .then((response) => {
+        return response.json()
+    })
+
+    .then(displayData)
+
+
+
+
+//Used to fetch the data from the api to display in the body when user enter the country name from the search form.
+document.getElementById('button1').addEventListener('click', function () {
+    fetch("https://covid-193.p.rapidapi.com/statistics", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "de8ca97b57mshbc75b832708333fp1b73aejsn44cd7debaa77",
+            "x-rapidapi-host": "covid-193.p.rapidapi.com"
+        }
+    })
+        .then((response) => {
+            return response.json()
+        })
+
+        .then((jsonData) => {
+
+            for (i = 0; i < jsonData.response.length; i++) {
+
+                //checking whether the input data from the form is equall to the api data fetch from the response[i].country
+                if (jsonData.response[i].country == document.querySelector('input').value) {
+
+
+                    listing("Country Name : " + jsonData.response[i].country)
+                    listing("Total Cases : " + jsonData.response[i].cases.total)
+                    listing("Total Recovered : " + jsonData.response[i].cases.recovered)
+                    listing("Total Death : " + jsonData.response[i].deaths.total)
+
+                    document.querySelector('input').value = "";
+
+                    //to make sure the clear button is created only when we have child nodes 4
+                    if (document.getElementById('detail').childNodes.length === 4) {
+
+                        const x = document.createElement("button")
+
+                        x.setAttribute('content', 'clear')
+                        x.textContent = "clear";
+                        document.querySelector('p').appendChild(x)
+
+                        //to clear the itmems from the ul that user has search from the search form.
+                        x.addEventListener('click', () => {
+                            location.reload();
+                        })
+
+                    }
+
+                }
+
+            }
+
+        })
 })
 
-.then(displayData)
 
-//This is the main function to display when the page reload.
-function displayData(jsonData){
-    
-    
-    for(i=0;i<jsonData.response.length;i++){
-        
+//Used to list the Covid-case data when user search from search form.
+function listing(data) {
+
+    const list = document.createElement('li')
+
+    list.textContent = data
+
+    document.getElementById("detail").appendChild(list);
+
+}
+
+
+//Used to display when the page reload.
+function displayData(jsonData) {
+
+
+    for (i = 0; i < jsonData.response.length; i++) {
+
         let tableRow = document.createElement('tr')
         document.querySelector('table').append(tableRow)
 
@@ -47,60 +98,13 @@ function displayData(jsonData){
         tableRow.appendChild(tableData2)
         tableRow.appendChild(tableData3)
         tableRow.appendChild(tableData4)
-    
-        tableData1.innerHTML = jsonData.response[i].country 
+
+        tableData1.innerHTML = jsonData.response[i].country
         tableData2.innerHTML = jsonData.response[i].cases.total
         tableData3.innerHTML = jsonData.response[i].deaths.total
         tableData4.innerHTML = jsonData.response[i].cases.recovered
-    
+
     }
 }
-
-//When search button is entered it will display the stat for that country only.
-document.getElementById('button1').addEventListener('click',function (){
-    fetch("https://covid-193.p.rapidapi.com/statistics", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "de8ca97b57mshbc75b832708333fp1b73aejsn44cd7debaa77",
-		"x-rapidapi-host": "covid-193.p.rapidapi.com"
-	}
-})
-.then((response)=>{
-   return response.json()
-})
-
-.then((jsonData)=>{
-    
-    for(i=0;i<jsonData.response.length;i++){
-      if  (jsonData.response[i].country==document.querySelector('input').value) {
-    
-        const x = document.createElement("button")
-        x.setAttribute('content','clear')
-        x.textContent = "clear";
-        document.querySelector('p').append(x)
-
-
-            listing("Country Name : " + jsonData.response[i].country )
-            listing("Total Cases : " + jsonData.response[i].cases.total)
-            listing("Total Recovered : " + jsonData.response[i].cases.recovered)
-            listing("Total Death : " + jsonData.response[i].deaths.total)
-        
-            document.querySelector('input').value = "";
-
-           
-
-            
-            
-
-
-        
-       }
-       
-     }
-    
-    })
-    
-})
-
 
 
